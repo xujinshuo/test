e59ae4d19d6738bd21f6f3eca2d728cd75f97839
xujinshuo <xujinshuo@zongluMacBookPro.local>
2018-09-11 16:41:47 +0800

    test11

--- manager/demo/page/qconf_page.h
+++
@@ -1,9 +0,0 @@
-#ifndef QCONF_PAGE_H
-#define QCONF_PAGE_H
-
-#include <string>
-#include <vector>
-
-void generate(std::string path, std::string value, std::string idc, std::vector<std::string> idcs, std::vector<std::string> children, std::string target);
-
-#endif
